c18f36064da850e918ae3d82b5ce1fafea9c67e1
vladISlove08 <artemenkovladislav92@gmail.com>
2024-02-01 19:34:10 +0100

    13

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
 function fuckyou() {
     alert("Fuck you slutt")
 };
+
+import photo from './img/caniuse-bg-dark.png';
+
+export default function App() {
+    return <img src={photo} alt="" />;
+  }
