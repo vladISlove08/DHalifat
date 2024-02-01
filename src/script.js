15d25c773ea4ef29834eebae3f621b301802be44
vladISlove08 <artemenkovladislav92@gmail.com>
2024-02-01 20:32:24 +0100

    v16.4

--- src/script.js
+++ src/script.js
@@ -6,15 +6,11 @@ function doSomething() {
   
   if (document.readyState === "loading") {
     // Loading hasn't finished yet
-    setTimeout(() => {  document.addEventListener("DOMContentLoaded", doSomething); console.log("succes1"); }, 2000);
-    
+    document.addEventListener("DOMContentLoaded", doSomething);
   } else {
     // `DOMContentLoaded` has already fired
-    setTimeout(() => {  doSomething(); console.log("succes2"); }, 2000);
-    
+    doSomething();
   };
 
   
-
-
 
